131e4c3bf6618c6061a09c0eb223faa3f6eff409
salahmohsen <salahmmohsen@gmail.com>
2023-06-18 19:10:55 +0300

    finalizing Pizza Project

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -82,16 +82,15 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={pizzaObj.soldOut ? "pizza sold-out" : "pizza"}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{!pizzaObj.soldOut ? "Available " : "Not available "}</span>
-        <span>{Number(pizzaObj.price)}$</span>
+        <span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price + "$"}</span>
       </div>
     </li>
   );
