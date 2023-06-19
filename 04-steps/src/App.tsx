9d897d5c0b0d46ef9f3ab232862e8b2f8fdae222
salahmohsen <salahmmohsen@gmail.com>
2023-06-19 22:21:29 +0300

    init steps App

---
+++ 04-steps/src/App.tsx
@@ -0,0 +1,76 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+// // Selecting DOM elements
+// const step1 = document.querySelector(".step-1");
+// const step2 = document.querySelector(".step-2");
+// const step3 = document.querySelector(".step-3");
+// const message = document.querySelector(".message");
+// const btnPrevious = document.querySelector(".previous");
+// const btnNext = document.querySelector(".next");
+
+// // "State"
+// let step = 1;
+
+// // Manually updating the DOM: changing text content and adding/removing classes (imperative approach)
+// const updateUIValues = function () {
+//   message.textContent = `Step ${step}: ${messages[step - 1]}`;
+
+//   if (step >= 1) step1.classList.add("active");
+//   else step1.classList.remove("active");
+
+//   if (step >= 2) step2.classList.add("active");
+//   else step2.classList.remove("active");
+
+//   if (step >= 3) step3.classList.add("active");
+//   else step3.classList.remove("active");
+// };
+
+// // Initial setup
+// updateUIValues();
+
+// // Manually attaching event listeners
+// btnPrevious.addEventListener("click", function () {
+//   if (step > 1) step -= 1;
+//   updateUIValues();
+// });
+
+// btnNext.addEventListener("click", function () {
+//   if (step < 3) step += 1;
+//   updateUIValues();
+// });
+
+export default function App() {
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`step-1, ${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`step-2, ${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`step-3, ${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          className="previous"
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Previous
+        </button>
+        <button
+          className="next"
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
