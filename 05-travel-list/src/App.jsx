ef3720678ca3a64b129164dfe00e73efb0605bba
salahmohsen <salahmmohsen@gmail.com>
2023-06-24 05:05:07 +0300

    70. Building the Layout

---
+++ 05-travel-list/src/App.jsx
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em> You have X items on your list, and you already packed x (X%) </em>
+    </footer>
+  );
+}
