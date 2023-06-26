2e5f991749c691e64d0bd9fef8a345b701b57d2c
salahmohsen <salahmmohsen@gmail.com>
2023-06-26 19:43:34 +0300

    82. Deleting items

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -7,11 +7,14 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItems(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteButton={handleDeleteItems} />
       <Stats />
     </div>
   );
@@ -54,25 +57,25 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingList({ items }) {
+function PackingList({ items, onDeleteButton }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteButton={onDeleteButton} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteButton }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteButton(item.id)}>❌</button>
     </li>
   );
 }
