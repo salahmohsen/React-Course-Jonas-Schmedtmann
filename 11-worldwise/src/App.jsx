037049377071e4bb83d2c5f21677b548b192094a
salahmohsen <salahmmohsen@gmail.com>
2023-08-10 22:42:39 +0300

    Nested Routes and Index Route

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -10,11 +10,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>Home</p>} />
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>List of Countries</p>} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- 11-worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyrights}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
