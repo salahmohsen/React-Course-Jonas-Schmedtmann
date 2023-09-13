a33374432dff4e74a439adf4db532a4d3a9649b1
salahmohsen <salahmmohsen@gmail.com>
2023-09-13 21:18:15 +0300

    Styling Form Elements

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -60,12 +60,18 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring-1 focus:ring-yellow-400 md:px-6 md:py-3"
+            />
           </div>
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-600 focus:outline-none focus:ring-1 focus:ring-yellow-400 focus:ring-offset-2 md:px-6 md:py-3"
             type="checkbox"
             name="priority"
             id="priority"
