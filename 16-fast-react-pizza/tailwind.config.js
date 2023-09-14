01aa4d078b514d4d24ba37faa94872e8e9847411
salahmohsen <salahmmohsen@gmail.com>
2023-09-14 17:16:36 +0300

    Add Roboto Mono font and edit tailwind confg file

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
