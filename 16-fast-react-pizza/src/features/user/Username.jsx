7186a85c5fb0dda48d2cd2a118d4d69d40ec246c
salahmohsen <salahmmohsen@gmail.com>
2023-09-15 02:11:20 +0300

    Modeling the User State With Redux Toolkit

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Salah</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
--- 16-fast-react-pizza/src/features/user/userSlice.js
+++ 16-fast-react-pizza/src/features/user/userSlice.js
@@ -1,3 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+/*
+import userSlice from '../../../final/final-2-final/src/features/user/userSlice';
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -19,3 +23,19 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+const initialState = { username: "salah" };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+export default userSlice.reducer;
