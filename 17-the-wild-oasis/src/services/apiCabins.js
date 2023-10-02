6cd3b434124f2e3195e90327c4dd610e382e9f9f
salahmohsen <salahmmohsen@gmail.com>
2023-10-03 02:43:24 +0300

    Connecting Supabase With Our React App

---
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,10 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
+}
