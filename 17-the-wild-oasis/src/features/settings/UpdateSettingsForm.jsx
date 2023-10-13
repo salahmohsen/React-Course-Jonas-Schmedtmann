e55495a76dd0bc6b502a48e510eee5eb17d1d2a2
salahmohsen <salahmmohsen@gmail.com>
2023-10-13 16:36:10 +0300

    Updating Application Settings

--- 17-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ 17-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
   const {
@@ -15,20 +16,42 @@ function UpdateSettingsForm() {
     } = {},
     error,
   } = useSettings();
+  const { isUpdatingSetting, updateSetting } = useUpdateSetting();
   if (isSettingsLoading) return <Spinner />;
+
+  function handleUpdate(e, fieldName) {
+    const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [fieldName]: value });
+  }
+
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLenth} />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLenth}
+          onBlur={(e) => handleUpdate(e, "minBookingLenth")}
+          disabled={isUpdatingSetting}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+          disabled={isUpdatingSetting}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
+          disabled={isUpdatingSetting}
         />
       </FormRow>
       <FormRow label="Breakfast price">
@@ -36,6 +59,8 @@ function UpdateSettingsForm() {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
+          disabled={isUpdatingSetting}
         />
       </FormRow>
     </Form>
---
+++ 17-the-wild-oasis/src/features/settings/useUpdateSetting.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+import { toast } from "react-hot-toast";
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdatingSetting } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Setting successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { updateSetting, isUpdatingSetting };
+}
